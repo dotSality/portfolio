4059c1a49a041c6a245fe5cc780a5a5f97a84a3c
dotSality <90727519+dotSality@users.noreply.github.com>
2022-01-04 15:10:01 +0300

    cosmetic changes

--- src/Projects/Project/Project.tsx
+++ src/Projects/Project/Project.tsx
@@ -1,23 +1,26 @@
 import s from './Project.module.css'
 import {ItemTitle} from '../../common/components/Titles/h3/ItemTitle';
 
+type StyleType = {
+    backgroundImage: string
+}
+
 type ProjectPropsType = {
     title: string,
     description: string,
+    style: StyleType,
 }
 
 export const Project = (props: ProjectPropsType) => {
     return (
         <div className={s.item}>
-            <div className={s.imageContainer}>
+            <div style={props.style} className={s.imageContainer}>
                 <a href={''} className={s.previewButton}>Click to preview</a>
             </div>
-            <div className={s.description}>
                 <ItemTitle title={props.title}/>
                 <span className={s.description}>
                     {props.description}
                 </span>
-            </div>
         </div>
     )
 }
--- src/Projects/Projects.tsx
+++ src/Projects/Projects.tsx
@@ -2,18 +2,24 @@ import s from './Projects.module.css'
 import {Project} from './Project/Project';
 import s_c from './../common/styles/Container.module.css'
 import {Title} from '../common/components/Titles/h2/Title';
+import preview from './../assets/image/no-preview.jpg';
 
 export const Projects = () => {
+    const image = {
+        backgroundImage: `url(${preview})`
+    }
+
+
     return (
         <div className={s.mainBlock}>
             <div className={`${s_c.contentContainer} ${s.container}`}>
                 <Title title={'My projects'}/>
                 <div className={s.itemContainer}>
-                    <Project title={'Project 1'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' +
+                    <Project style={image} title={'Project 1'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' +
                                                             ' Amet dolor dolore ea nam necessitatibus odit ratione r' +
                                                             'em, tempora vero? Consequatur facilis illum inc' +
                                                             'idunt magnam praesentium quidem voluptas! Aliquam mollitia, odio?\n'}/>
-                    <Project title={'Project 2'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' +
+                    <Project style={image} title={'Project 2'} description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' +
                                                             ' Amet dolor dolore ea nam necessitatibus odit ratione r' +
                                                             'em, tempora vero? Consequatur facilis illum inc' +
                                                             'idunt magnam praesentium quidem voluptas! Aliquam mollitia, odio?\n'}/>
